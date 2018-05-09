f748ade8f2a1eda77da551fad2275f0a433c7dec
BlairCorban <blaircorban@hotmail.com>
2018-05-09 13:39:50 +1200

    deleted unneeded things

--- CatchingAnimals/Assets/daynightcycle.cs
+++ CatchingAnimals/Assets/daynightcycle.cs
@@ -8,7 +8,7 @@ public class daynightcycle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.RotateAround(Vector3.zero, Vector3.right, 5f*Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.right, 0.5f*Time.deltaTime);
         transform.LookAt(Vector3.zero);
 	}
 }
