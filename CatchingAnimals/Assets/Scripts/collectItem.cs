35eaa06e6298c398ce2d051038e6aa76a17248f2
BlairCorban <blaircorban@hotmail.com>
2018-05-09 14:07:21 +1200

    added crosshair and moved scripts to folder

---
+++ CatchingAnimals/Assets/Scripts/collectItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectItem : MonoBehaviour {
+    public Camera camera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 5))
+            {
+                Transform objectHit = hit.transform;
+                if(objectHit.name == "wood" || objectHit.name == "wood(Clone)")
+                {
+                    Destroy(objectHit.gameObject);
+                    stats.woodInventory += 1;
+                }
+                else if(objectHit.name == "house")
+                {
+                    if(stats.woodInventory >= 5)
+                    {
+
+                        objectHit.localScale = objectHit.localScale + objectHit.localScale;
+                        stats.woodInventory -= 5;
+                    }
+                }
+                
+            }
+        }
+	}
+}
---
+++ CatchingAnimals/Assets/Scripts/daynightcycle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class daynightcycle : MonoBehaviour {
+    public float rotationSpeed;
+	
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(Vector3.zero, Vector3.right, rotationSpeed * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+	}
+}
